3dc4f0b6e54894cdfc904e9e926bfc47cbb3f60d
tanadendere <tanadendere@gmail.com>
2024-07-10 10:39:49 +0200

    feat: :sparkles: Adding functionality to retrieve the images and removed daisy ui template code

--- src/apiUrls.ts
+++ src/apiUrls.ts
@@ -1,1 +1,5 @@
 export const apiBaseURL = "https://api.comick.io/";
+
+export function getImageURL(b2key: string): string {
+  return `https://meo3.comick.pictures/${b2key}`;
+}
--- src/search/components/SearchResultCard.tsx
+++ src/search/components/SearchResultCard.tsx
@@ -1,17 +1,17 @@
-function SearchResultCard({ searchResult }: { searchResult: string }) {
+import { getImageURL } from "../../apiUrls";
+import { IComic } from "../../models/comicData";
+
+function SearchResultCard({ comic }: { comic: IComic }) {
   return (
     <div className="card bg-base-100 image-full w-96 shadow-xl">
       <figure>
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-          alt="Shoes"
-        />
+        <img src={getImageURL(comic.md_covers[0]?.b2key)} alt="" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">{searchResult}</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <h2 className="card-title">{comic.title}</h2>
+        <p>{comic.desc}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-primary">Read Now</button>
         </div>
       </div>
     </div>
